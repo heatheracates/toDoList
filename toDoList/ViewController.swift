3b7d74acdd8b8c87ab2935abf8777caff77fc9eb
Heather Cates <cates.heather.a@gmail.com>
2017-11-24 21:20:16 -0800

    empty list created

--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -8,11 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate,UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    //how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    //what goes in the row 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
